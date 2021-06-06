c0a7337e7bc4ce050b0f88a3dc09c98f9a37d883
samgithub1997 <samarthsharma351@gmail.com>
2021-06-06 08:01:46 +0530

    Nav bar and Search : Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import SearchIcon from '@material-ui/icons/Search';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <h1>Yo Lets start building...</h1>  */}
+        <Header />
+    </div>
+  );
+}
+
+export default App;
